3ea5aa77dd863a32de13996030d960bc4e58c235
amitkumar2448 <amitkumar2448@gmail.com>
2026-03-29 22:01:18 +0530

    Refactor project to use MongoDB instead of MySQL; update dependencies and configuration

--- src/main/java/com/study/roadmap/entity/MyUser.java
+++ src/main/java/com/study/roadmap/entity/MyUser.java
@@ -1,26 +1,23 @@
 package com.study.roadmap.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
-@Table(name = "users")
+import lombok.Data;
+
+@Document(collection = "users")
+@Data
 public class MyUser {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
-    @Column(name = "username")
+   
     private String username;
 
-    @Column(name = "password")
+
     private String password;
 
-    @Column(name = "role")
+
     private String role;
 }
--- src/main/java/com/study/roadmap/repository/UserRepository.java
+++ src/main/java/com/study/roadmap/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.study.roadmap.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.study.roadmap.entity.MyUser;
 
 @Repository
-public interface UserRepository extends JpaRepository<MyUser, Long> {
-    
+public interface UserRepository extends MongoRepository<MyUser, String> {
+    // You can define custom query methods here if needed
 }
