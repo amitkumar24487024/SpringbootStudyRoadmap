b0f3aa0bfc8d1fa2aeb30eaca823696e203e5e3a
amitkumar2448 <amitkumar2448@gmail.com>
2026-03-29 22:55:28 +0530

    Add @Service annotation to UserService and update MySQL datasource URL

--- src/main/java/com/study/roadmap/service/UserService.java
+++ src/main/java/com/study/roadmap/service/UserService.java
@@ -3,10 +3,12 @@ package com.study.roadmap.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.study.roadmap.entity.MyUser;
 import com.study.roadmap.repository.UserRepository;
 
+@Service
 public class UserService {
     
     private UserRepository userRepository;
